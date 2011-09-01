2afae37b69937bf9a53ad3bdffe67ea2653d196c
William Bert <william.bert@gmail.com>
2011-09-01 12:58:23 -0400

    Added _get_starttime and get_untiltime methods. Did some work on HOURLY, MINUTELY, and SECONDLY.

--- human_rrule/human_rrule2.py
+++ human_rrule/human_rrule2.py
@@ -15,7 +15,7 @@ import unittest
 from datetime import datetime, time
 
 # import dateutil
-from dateutil.rrule import YEARLY, MONTHLY, DAILY, HOURLY, MINUTELY, SECONDLY 
+from dateutil.rrule import YEARLY, MONTHLY, WEEKLY, DAILY, HOURLY, MINUTELY, SECONDLY 
 from dateutil.rrule import MO, TU, WE, TH, FR, SA, SU, weekdays
 from dateutil.rrule import weekday
 from dateutil.rrule import rrule as rr
@@ -112,6 +112,7 @@ WEEKDAY_LONG_MAP = (
 
 DEFAULT_DATE_FORMAT = "%B %d, %Y"
 DEFAULT_TIME_FORMAT = "%I:%M %p"
+DEFAULT_DATETIME_FORMAT = " ".join([DEFAULT_TIME_FORMAT, DEFAULT_DATE_FORMAT])
 
         
 class human_rrule(dict):
@@ -119,7 +120,8 @@ class human_rrule(dict):
     
     Keys: 
     frequency = YEARLY, MONTHLY (interval, represented by period)
-    period = of the year, of the month, etc.
+    occurrence = 
+    period = of the year, of the month, etc. Don't need for HOURLY, MINUTELY, or WEEKLY.
     interval = each, every other, 
     occurrence = eg, first Sunday, third Friday, tenth day, second hour, seventh second, etc.
     begin_time = the start time of each occurrence
@@ -143,24 +145,26 @@ class human_rrule(dict):
     rrule = property(get_rrule, set_rrule)
     
     def get_description(self, date_format=DEFAULT_DATE_FORMAT, time_format=DEFAULT_TIME_FORMAT):
-        """Returns a string consisting of all the values of an human_rrule in 
+        """Convenience method for returning a string consisting of all the values of an human_rrule in 
         an order that reflects an English language description of the rrule."""
         
+        datetime_format = " ".join([time_format, date_format])
         desc = []
         # desc.append(self["frequency"])
         desc.append(self["interval"])
         desc.append(self["occurrence"])
         desc.append(self["period"])
-        desc.append("starting at %s" % self.__rrule._timeset[0].strftime(time_format)) # TODO fix for multiple times in _timeset--when would this be the case?
+        desc.append("starting at %s" % self._get_starttime().strftime(datetime_format)) 
         if self["terminal"].startswith("until"):            
-            desc.append(' '.join(["until", self.__rrule._until.strftime(date_format)]))
+            untiltime = self._get_untiltime()
+            desc.append(' '.join(["until", untiltime.strftime(datetime_format) if untiltime else ""]))
         else:
             desc.append(self["terminal"])
 
         if self["timezone"]:
             desc.append("in the %s time zone" % self["timezone"])
 
-        return " ".join(desc)
+        return " ".join(desc).replace("  ", " ")
         
     def __unicode__(self):
         return unicode(self.get_description())
@@ -174,23 +178,23 @@ class human_rrule(dict):
             raise ValueError, "Invalid frequency in rrule: %s" % rr._freq
         freq = rr._freq # when the recurrence recurs, secondly through yearly. Required.
         interval = rr._interval # how often the recurrence happens, each time through every nth time. Defaults to 1.
-        wkst = rr._wkst # Week start day, ie, an int representing which day of the week starts the week, usually Sunday or Monday. Defaults to calendar.firstweekday().
+        wkst = rr._wkst # Integer representing week start day, ie, an int representing which day of the week starts the week, usually 0 for Sunday or 1 for Monday. Defaults to calendar.firstweekday().
         until = rr._until # datetime until which the recurrence continues. Only it or count is set, not both.
         count = rr._count # Number of times the event happens before it stops. Only it or until is set, not both.
         tzinfo = rr._tzinfo # Time zone information. Defaults to the tzinfo of dtstart.
-        bymonth = rr._bymonth # Which month a yearly event recurs in.
-        byweekno = rr._byweekno # Which week number a yearly event recurs in.
-        byyearday = rr._byyearday # Which day of the year a yearly event recurs in.
-        byweekday = rr._byweekday # Which weekday an event recurs in.
-        bynweekday = rr._bynweekday # By the nth weekday, e.g., FR(3) is the third Friday of the period
-        byeaster = rr._byeaster
-        bymonthday = rr._bymonthday # Relative day of the month
-        bynmonthday = rr._bynmonthday # Relative day of the month, if negative (counting from the end of the month)
-        bysetpos = rr._bysetpos # For sets of seconds/minutes/hours/days/weeks/months/years, specifies which position in the list to pay attention to.
-        byhour = rr._byhour
-        byminute = rr._byminute
-        bysecond = rr._bysecond
-        timeset = rr._timeset # The time the occurrence is set to. None if freq >= HOURLY
+        bymonth = rr._bymonth # Tuple. Which month a yearly event recurs in.
+        byweekno = rr._byweekno # Tuple. Which week number a yearly event recurs in.
+        byyearday = rr._byyearday # Tuple. Which day of the year a yearly event recurs in.
+        byweekday = rr._byweekday # Tuple. Which weekday an event recurs in.
+        bynweekday = rr._bynweekday # Tuple. By the nth weekday, e.g., FR(3) is the third Friday of the period. Only used if freq is < MONTHLY
+        byeaster = rr._byeaster # Tuple.
+        bymonthday = rr._bymonthday # Tuple. Relative day of the month
+        bynmonthday = rr._bynmonthday # Tuple. Relative day of the month, if negative (counting from the end of the month)
+        bysetpos = rr._bysetpos # Tuple. For sets of seconds/minutes/hours/days/weeks/months/years, specifies which position in the list to pay attention to.
+        byhour = rr._byhour # Tuple. The hour of the occurrence.
+        byminute = rr._byminute # Tuple. The minutes of the occurrence.
+        bysecond = rr._bysecond # Tuple. The second of the occurrence.
+        timeset = rr._timeset # If freq < HOURLY and all three of hour, minute, and second are set, timeset is a tuple of datetime.times representing when the occurrence occurs. None if otherwise.
         # YEARLY needs to have bymonth and bymonthday set
         # MONTHLY needs to have bymonthday set 
         # WEEKLY needs to have byweekday set
@@ -202,15 +206,27 @@ class human_rrule(dict):
         # self["frequency"] = FREQUENCY_MAP[freq]
         
         # Initialize the period, which is derived from the frequency. 
-        self["period"] = ' '.join(["of the", PERIOD_MAP[freq]])
-        self["interval"] = INTERVAL_MAP[interval]
+        if freq in [HOURLY, MINUTELY, SECONDLY]:
+            self["period"] = "" # Expresing periods doesn't make sense for these frequencies (e.g., 
+        else:
+            self["period"] = ' '.join(["of the", PERIOD_MAP[freq]])
+        self["interval"] = INTERVAL_MAP[interval]       
  
         if bynweekday:
             # 
             pass
                 
         if freq == YEARLY:
-            pass
+            self["occurrence"] = ""
+            if byyearday:
+                pass
+            if byweekno:
+                pass
+            if byweekday or bynweekday:
+                pass
+            if bysetpos:
+                pass
+            
         elif freq == MONTHLY:            
             # bynweekday is a tuple of (weekday, week_in_period) tuples
             for rule_pair in bynweekday:
@@ -224,36 +240,55 @@ class human_rrule(dict):
                 name = [WEEKDAY_MAP[unicode(p_weekday)]]
                 ord_text.extend(name)
                 self["occurrence"] = " ".join(ord_text)                
-                
-                self["begin_time"] = " ".join(["starting at", str(timeset)])
                                         
         elif freq == WEEKLY:
+            self["occurrence"] = ""
             # check wkst to see which day of week is first
             pass
             
         elif freq == DAILY:
+            self["occurrence"] = ""
             pass              
             
         elif freq == HOURLY:
-            pass               
+            self["occurrence"] = "hour"
 
         elif freq == MINUTELY:
-            pass                
+            self["occurrence"] = "minute"
 
         elif freq == SECONDLY:
-            pass               
+            self["occurrence"] = "second"
+            if bymonthday:                  
+                s = "".join(["of the ", human_rrule.int_as_ordinal(bymonthday[0]), " day of the month"]) # TODO work with multiple bymonthdays
+                self["occurrence"] = " ".join([self["occurrence"], s])
+
 
         else:
             raise human_rruleError, "Frequency value of %s is not valid." % freq
+            
+        self["begin_time"] = " ".join(["starting at", self._get_starttime().strftime(DEFAULT_DATETIME_FORMAT)])
         
         if count:
             self["terminal"] = "%s times" % int2word(count).rstrip()
         elif until:
-            self["terminal"] = "until %s" % until.strftime(DEFAULT_DATE_FORMAT)
+            untiltime = self._get_untiltime()
+            self["terminal"] = "until %s" % untiltime.strftime(DEFAULT_DATETIME_FORMAT) if untiltime else ""
             
         self["timezone"] = tzinfo and "%s" % tzinfo or None
     
  
+    def _get_starttime(self):
+        """Get the actual starttime of the recurrence. dtstart is used as a boundary, but depending on the rules, it may or may not be the actual datetime when the first instance of the recurrence occurs."""
+        for d in self.__rrule:
+            return d
+
+    def _get_untiltime(self):
+        """Get the actual untiltime of the recurrence. until is used as a boundary, but depending on the rules, it may or may not be the actual datetime when the last instance of the recurrence occurs."""        
+        last = None
+        for d in self.__rrule:
+            last = d             
+        return last
+                
     @staticmethod
     def int_as_ordinal(i):
         """Return a string representing an int as an ordinal number."""
@@ -332,25 +367,48 @@ class human_rruleTests(unittest.TestCase):
         self.assertRaises(ValueError, human_rrule, rrule=testrr)
 
     def test_monthly(self):
-        correct = u"each third Friday of the month starting at 12:00 AM ten times"
-        testrr = rrule_eq(MONTHLY, byweekday=FR(3), dtstart=datetime(2011, 8, 15), count=10)
+        correct = u"each third Friday of the month starting at 12:01 AM August 19, 2011 ten times"
+        testrr = rrule_eq(MONTHLY, byweekday=FR(3), dtstart=datetime(2011, 8, 15, 0, 1), count=10)
         hr = human_rrule(testrr)
         self.assertEqual(hr.get_description(), correct)
         
-        correct = u"every other first Sunday of the month starting at 09:00 PM until August 15, 2012"
+        correct = u"every other first Sunday of the month starting at 09:00 PM October 02, 2011 until 09:00 PM August 05, 2012"
         testrr = rrule_eq(MONTHLY, interval=2, byweekday=SU(1), dtstart=datetime(2011, 8, 15, 21, 0, 0), until=datetime(2012, 8, 15))
         hr = human_rrule(testrr)
         self.assertEqual(hr.get_description(), correct)
             
-        correct = u"every other first Sunday of the month starting at 09:00 PM until 08/15/2012"
+        correct = u"every other first Sunday of the month starting at 09:00 PM 10/02/2011 until 09:00 PM 08/05/2012"
         self.assertEqual(hr.get_description(date_format="%m/%d/%Y"), correct)
         
-        correct = u"every other first Sunday of the month starting at 21:00 until August 15, 2012"
+        correct = u"every other first Sunday of the month starting at 21:00 October 02, 2011 until 21:00 August 05, 2012"
         self.assertEqual(hr.get_description(time_format="%H:%M"), correct)
         
+    def test_secondly(self):
+        correct = u"each second starting at 00:00:00 AM August 15, 2011 ten times"
+        testrr = rrule_eq(SECONDLY, dtstart=datetime(2011, 8, 15), count=10)
+        hr = human_rrule(testrr)
+        self.assertEqual(hr.get_description(time_format="%H:%M:%S %p"), correct)
+        
+        correct = "each second of the twenty first day of the month starting at 00:00:00 AM August 21, 2011 ten times"
+        testrr = rrule_eq(SECONDLY, dtstart=datetime(2011, 8, 15), count=10, bymonthday=21)
+        hr = human_rrule(testrr)
+        self.assertEqual(hr.get_description(time_format="%H:%M:%S %p"), correct)
+        
+    def test_minutely(self):
+        correct = u"each minute starting at 00:00:00 AM August 15, 2011 ten times"
+        testrr = rrule_eq(MINUTELY, dtstart=datetime(2011, 8, 15), count=10)
+        hr = human_rrule(testrr)
+        self.assertEqual(hr.get_description(time_format="%H:%M:%S %p"), correct)
+        
+    def test_hourly(self):
+        correct = u"each hour starting at 00:00:00 AM August 15, 2011 ten times"
+        testrr = rrule_eq(HOURLY, dtstart=datetime(2011, 8, 15), count=10)
+        hr = human_rrule(testrr)
+        self.assertEqual(hr.get_description(time_format="%H:%M:%S %p"), correct)        
+        
     def test_yearly(self):
-        correct = u"Each third Friday of the year starting at 12:00 AM ten times"
-        testrr = rrule_eq(YEARLY, byweekday=FR(3), dtstart=datetime(2011, 8, 15), count=10)
+        correct = u"each August 15 of the year starting at 00:00 AM August 15, 2011 ten times"
+        testrr = rrule_eq(YEARLY, dtstart=datetime(2011, 8, 15), count=10)
         hr = human_rrule(testrr)
         self.assertEqual(hr.get_description(), correct)
         
--- human_rrule/rrule_eq.py
+++ human_rrule/rrule_eq.py
@@ -14,13 +14,28 @@ import unittest
 from datetime import datetime, time
 
 from dateutil.rrule import rrule as rr
-from dateutil.rrule import YEARLY, MONTHLY, DAILY, HOURLY, MINUTELY, SECONDLY 
+from dateutil.rrule import YEARLY, MONTHLY, WEEKLY, DAILY, HOURLY, MINUTELY, SECONDLY 
 from dateutil.rrule import MO, TU, WE, TH, FR, SA, SU, weekdays
 from dateutil.rrule import weekday
 
 
 class rrule_eq(rr): 
     """Wrapper class around an rrule that provides __eq__ and __ne__ methods."""
+    
+    # This is for debugging only
+    def __init__(self, freq, dtstart=None,
+                 interval=1, wkst=None, count=None, until=None, bysetpos=None,
+                 bymonth=None, bymonthday=None, byyearday=None, byeaster=None,
+                 byweekno=None, byweekday=None,
+                 byhour=None, byminute=None, bysecond=None,
+                 cache=False):
+        rr.__init__(self, freq, dtstart=dtstart,
+                 interval=interval, wkst=wkst, count=count, until=until, bysetpos=bysetpos,
+                 bymonth=bymonth, bymonthday=bymonthday, byyearday=byyearday, byeaster=byeaster,
+                 byweekno=byweekno, byweekday=byweekday,
+                 byhour=byhour, byminute=byminute, bysecond=bysecond,
+                 cache=cache) # rrule is an old-style class
+        self._pprint()
         
     def __eq__(self, other):
         """Compare two human_rrule instances."""
